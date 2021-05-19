260768805a7b84e8e3fb437916c539e9a37c02d9
Игорь Перминов <Perminov.I.Yurye@sberbank.ru>
2021-05-19 17:04:16 +0300

    фикс текстовки и дизайна

--- demo/app/src/main/java/ru/sberdevices/pub/demoapp/ui/smartapp/ui/SmartAppFragment.kt
+++ demo/app/src/main/java/ru/sberdevices/pub/demoapp/ui/smartapp/ui/SmartAppFragment.kt
@@ -1,10 +1,12 @@
 package ru.sberdevices.pub.demoapp.ui.smartapp.ui
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.text.HtmlCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -36,6 +38,11 @@ class SmartAppFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.descriptionTextView.text = Html.fromHtml(
+            getString(R.string.asistant_description),
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+        
         view.findViewById<ImageView>(R.id.androidImageView).setOnClickListener { viewModel.sendServerAction() }
     }
 
